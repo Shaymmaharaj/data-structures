2f606f3f68991d6c08649ba69b79299b5b390c2f
shaymmaharaj <shyammaharaj@gmail.com>
2019-04-10 16:21:54 -0700

    first commit

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,29 @@
 var Stack = function() {
   var someInstance = {};
-
+  var num = 0
   // Use an object with numeric keys to store values
   var storage = {};
+  
+
 
   // Implement the methods below
   someInstance.push = function(value) {
-  };
+    // 
+    num++
+    storage[num] = value 
+  
+};
 
   someInstance.pop = function() {
+  
+    
+
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length
+    
   };
-
+  console.log(someInstance)
   return someInstance;
 };
